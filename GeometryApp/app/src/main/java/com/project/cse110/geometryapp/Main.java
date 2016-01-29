7d09683c3b4b00eec647cb1a358757c8acc71498
devin.hickey <dchickey@ucsd.edu>
2016-01-29 15:09:13 -0800

    Merged Main and XML

--- GeometryApp/app/src/main/java/com/project/cse110/geometryapp/Main.java
+++ GeometryApp/app/src/main/java/com/project/cse110/geometryapp/Main.java
@@ -1,15 +1,12 @@
 package com.project.cse110.geometryapp;
 
 import android.app.Activity;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by devinhickey on 1/29/16.
@@ -21,89 +18,28 @@ public class Main extends Activity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     //private GoogleApiClient client;
-
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         System.out.println("Inside OnCreate main");
         setContentView(R.layout.content_main);
 
-        Button first = (Button) findViewById(R.id.firstButton);
-        Button second = (Button) findViewById(R.id.secondButton);
+        Button first = (Button) findViewById(R.id.topic1);
+        Button second = (Button) findViewById(R.id.topic2);
         first.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        TextView myTextView = (TextView) findViewById(R.id.myTextView);
-                        myTextView.setText("First Button clicked");
+                        Intent topic1Intent = new Intent(Main.this, topic1.class);
+                        startActivity(topic1Intent);
                     }
                 }
         );
         second.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        TextView myTextView = (TextView) findViewById(R.id.myTextView);
-                        myTextView.setText("Second Button clicked");
                     }
                 }
         );
 
-          /*
-            @Override
-            public void onClick(View v) {
-                System.out.println("First Button Clicked");
-
-            }
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-
-    public void onClick(View v) {
-        R.id.firstButton = (Button) v;
-        ((Button) v).setText("clicked");
-        /*if (v == R.id.firstButton)
-            System.out.println("First button clicked");
-        */
-    }
-/*
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.project.cse110.geometryapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.project.cse110.geometryapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }*/
 }
