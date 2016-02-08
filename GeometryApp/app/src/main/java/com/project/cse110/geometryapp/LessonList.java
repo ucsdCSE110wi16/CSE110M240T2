e4931b0876028def2eee640a54890659d97b090a
devin.hickey <dchickey@ucsd.edu>
2016-02-07 22:33:03 -0800

    Added Lesson 2 Description

--- GeometryApp/app/src/main/java/com/project/cse110/geometryapp/LessonList.java
+++ GeometryApp/app/src/main/java/com/project/cse110/geometryapp/LessonList.java
@@ -29,11 +29,11 @@ public class LessonList extends Activity {
                     public void onClick(View v) {
                         Intent newIntent = new Intent(LessonList.this, LessonDescription.class);
                         // Grab text from xml FIXME
-                        String intentString = "Lines: A line is a straight one-dimensional geometric object that extends infinitely in both directions.\n"
+                        String intentString = "-Lines: A line is a straight one-dimensional geometric object that extends infinitely in both directions.\n\n"
                                 +
-                                "Line Segment: A line segment is a straight one-dimensional geometric object that has fixed starting and ending points.\n"
+                                "-Line Segment: A line segment is a straight one-dimensional geometric object that has fixed starting and ending points.\n\n"
                                 +
-                                "Ray: A ray is a straight one-dimensional geometric object that has a fixed starting point, but extends infinitely.";
+                                "-Ray: A ray is a straight one-dimensional geometric object that has a fixed starting point, but extends infinitely.";
 
                         newIntent.putExtra("body", intentString);
 
@@ -47,7 +47,10 @@ public class LessonList extends Activity {
                 public void onClick(View v) {
                     Intent newIntent = new Intent(LessonList.this, LessonDescription.class);
 
-                    String intentString = "Other Description";
+                    String intentString = "-Point: A point is a geometric object that signifies a location, but has no size in itself. Lines, line segments, rays are all collections of points.\n\n"
+                            + "-Plane: A plane is a flat, two-dimensional surface that extends infinitely far. It is the two-dimensional analogue of a point (zero dimensions), a line (one dimension).\n\n"
+                            + "-Collinear Points: Points are collinear if they lie on the same line.\n\n"
+                            + "-Coplanar Points: Points are coplanar if they both lie on the same plane.";
                     newIntent.putExtra("body", intentString);
                     startActivity(newIntent);
 
