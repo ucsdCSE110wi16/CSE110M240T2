277d942da77d1ec7f6d76f37f1ca7d307ea2662e
Migal- <mi.ucsd10@gmail.com>
2016-03-04 15:10:57 -0800

    Created context member variable in Pref

--- GeometryApp/app/src/main/java/com/project/cse110/geometryapp/Preferences.java
+++ GeometryApp/app/src/main/java/com/project/cse110/geometryapp/Preferences.java
@@ -10,10 +10,11 @@ import com.firebase.client.Firebase;
  * Created by Migal on 2/29/2016.
  */
 public class Preferences {
+    private Context context;
 
-    public Preferences(){}
+    public Preferences(Context context){this.context = context;}
 
-    public void storeUserInfo(User user, Context context){
+    public void storeUserInfo(User user){
         SharedPreferences user_info = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = user_info.edit();
         editor.putString("uid", user.getUid());
@@ -22,32 +23,32 @@ public class Preferences {
         editor.apply();
     }
 
-    public String retrieveUIDInfo(Context context){
+    public String retrieveUIDInfo(){
         SharedPreferences user_info = PreferenceManager.getDefaultSharedPreferences(context);
         return user_info.getString("uid", "");
     }
 
-    public String retrieveEmailInfo(Context context){
+    public String retrieveEmailInfo(){
         SharedPreferences user_info = PreferenceManager.getDefaultSharedPreferences(context);
         return user_info.getString("email", "");
     }
 
-    public Firebase retrieveRefInfo(Context context){
+    public Firebase retrieveRefInfo(){
         SharedPreferences user_info = PreferenceManager.getDefaultSharedPreferences(context);
         String ref = user_info.getString("ref", "");
         return new Firebase(ref);
     }
 
-    public User retrieveUserInfo(Context context){
-        return new User(retrieveUIDInfo(context), retrieveEmailInfo(context), retrieveRefInfo(context));
+    public User retrieveUserInfo(){
+        return new User(retrieveUIDInfo(), retrieveEmailInfo(), retrieveRefInfo());
     }
 
-    public boolean checkForUserInfo(Context context){
+    public boolean checkForUserInfo(){
         SharedPreferences user_info = PreferenceManager.getDefaultSharedPreferences(context);
         return user_info.contains("uid");
     }
 
-    public void clearUserInfo(Context context){
+    public void clearUserInfo(){
         SharedPreferences user_info = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = user_info.edit();
         editor.clear();
