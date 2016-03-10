f9de595e30c134e3a64879be9b1c984bf1489ee9
Masrhall Seid <Marchness@mm-2.dynamic.ucsd.edu>
2016-03-09 17:09:35 -0800

    Added scenario testing for User login

---
+++ GeometryApp/app/src/androidTest/java/com/project/cse110/geometryapp/TestLogin.java
@@ -0,0 +1,70 @@
+package com.project.cse110.geometryapp;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by Marchness on 3/9/16.
+ */
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+
+//Test 1: Login
+@LargeTest
+public class TestLogin {
+
+    private String usernameToType;
+    private String passwordToType;
+
+    @Rule
+    public ActivityTestRule<LoginScreen> mActivityRule = new ActivityTestRule<>(
+            LoginScreen.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        usernameToType = "t@t.com";
+        passwordToType = "test";
+        System.out.println("Hello I am initValid***************");
+
+    }
+
+    @Test
+    public void testUser() {
+        System.out.println("TESTING USER LOGIN..........");
+        // Type text and then press the button.
+        onView(withId(R.id.etUsername)).perform(typeText(usernameToType), closeSoftKeyboard());
+        onView(withId(R.id.etPassword)).perform(typeText(passwordToType), closeSoftKeyboard());
+
+        // Check that the text was changed
+        onView(withId(R.id.etUsername)).check(matches(withText(usernameToType)));
+        onView(withId(R.id.bLogin)).perform(click());
+        onView(withId(R.id.registerButton)).perform(click());
+
+    }
+/*
+    @Test
+    public void testTest() {
+        System.out.println("Hello I am in testtest***************");
+       // onView(withId(R.id.etUsername)).perform(typeText(usernameToType), closeSoftKeyboard());
+       // onView(withId(R.id.etUsername)).check(matches(withText(usernameToType)));
+
+    }
+    */
+}
+
+//Test 2:
